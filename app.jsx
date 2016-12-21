0fe4701142fb04cf3dc768b3bc30b38f2a198dd5
Andreas Moor <github@grmnd.ch>
2016-12-21 15:06:17 +0100

    added graphql + graphiql

--- app.jsx
+++ app.jsx
@@ -2,11 +2,24 @@
 
 import React from 'react';
 import { render } from 'react-dom';
+import GraphiQL from 'graphiql';
+import fetch from 'isomorphic-fetch';
+
+import './style.css'
+import 'graphiql/graphiql.css';
+
+function graphQLFetcher(graphQLParams) {
+  return fetch(window.location.origin + '/graphql', {
+    method: 'post',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(graphQLParams),
+  }).then(response => response.json());
+}
 
 class App extends React.PureComponent {
   render() {
     return (
-      <div>Hello, World!</div>
+      <GraphiQL fetcher={ graphQLFetcher } />
     );
   }
 }
--- server.js
+++ server.js
@@ -2,6 +2,10 @@ import webpack from 'webpack';
 import WebpackDevServer from 'webpack-dev-server';
 import config from './webpack.config';
 
+import Schema from './schema';
+import { graphql } from 'graphql';
+import bodyParser from 'body-parser';
+
 const {
   NODE_ENV,
   PORT = "3000"
@@ -10,7 +14,22 @@ const {
 config.entry.unshift(`webpack-dev-server/client?http://localhost:${PORT}/`);
 const compiler = webpack(config);
 
-const devServer = new WebpackDevServer(compiler);
+const devServer = new WebpackDevServer(compiler, {
+  setup(app) {
+    app.use(bodyParser.json());
+    app.use((req, res, next) => {
+      if(req.url === '/graphql' && req.method === "POST") {
+        // Executing the GraphQL query
+        const {query, variables} = req.body;
+        graphql(Schema, query, null, variables).then(result => {
+          res.send(result);
+        });
+      } else {
+        next();
+      }
+    });
+  }
+});
 
 devServer.listen(PORT, (err, result) => {
   if(err) {
