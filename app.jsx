e8f3762f1be883d6980858058ad878fe329b6970
Andreas Moor <github@grmnd.ch>
2017-01-05 16:27:48 +0100

    nested containers

--- app.jsx
+++ app.jsx
@@ -5,27 +5,60 @@ import ReactDOM from 'react-dom';
 
 import Relay from 'react-relay';
 
-let ProfileApp = (props) => {
-  const viewer = props.viewer;
-  return (
-    <div>
-      <select value={ props.relay.variables.uid }
-              onChange={ (e) => props.relay.setVariables({ uid: +e.target.value })}>
-        { viewer.users.map(u => <option key={u.uid} value={u.uid}>{u.name}</option> ) }
-      </select>
-      <h1>{ viewer.user.name }</h1>
-      <h2>Friends</h2>
-      <ul>
-        { viewer.user.friends.map(f => <li key={f.name}>{ f.name }</li>) }
-      </ul>
-      <h2>Pets</h2>
-      <ul>
-        { viewer.user.pets.edges.map(e => <li key={e.node.name}>{ e.node.name }</li>) }
-      </ul>
-    </div>
-  );
-}
-ProfileApp = Relay.createContainer(ProfileApp, {
+let UserButton = (props) =>
+  <button onClick={ () => alert(`User has ID ${props.user.uid}`)}>Alert!</button>
+UserButton = Relay.createContainer(UserButton, {
+  fragments: {
+    user: () => Relay.QL`
+      fragment on User {
+        uid
+      }
+    `
+  }
+})
+
+let UserInfo = (props) =>
+  <div>
+    <h1>{ props.user.name }</h1>
+    <h2>Friends</h2>
+    <ul>
+      { props.user.friends.map(f => <li key={f.name}>{ f.name }</li>) }
+    </ul>
+    <h2>Pets</h2>
+    <ul>
+      { props.user.pets.edges.map(e => <li key={e.node.name}>{ e.node.name }</li>) }
+    </ul>
+  </div>
+UserInfo = Relay.createContainer(UserInfo, {
+  fragments: {
+    user: () => Relay.QL`
+      fragment on User {
+        name
+        friends {
+          name
+        }
+        pets(first: 200) {
+          edges {
+            node {
+              name
+            }
+          }
+        }
+      }
+    `
+  }
+})
+
+let ProfileViewer = (props) =>
+  console.log(props) || <div>
+    <select value={ props.relay.variables.uid }
+            onChange={ (e) => props.relay.setVariables({ uid: +e.target.value })}>
+      { props.viewer.users.map(u => <option key={u.uid} value={u.uid}>{u.name}</option> ) }
+    </select>
+    <UserInfo user={props.viewer.user} />
+    <UserButton user={props.viewer.user} />
+  </div>
+ProfileViewer = Relay.createContainer(ProfileViewer, {
   initialVariables: {
     uid: 1
   },
@@ -38,17 +71,8 @@ ProfileApp = Relay.createContainer(ProfileApp, {
           name
         }
         user(uid: $uid) {
-          name
-          friends {
-            name
-          }
-          pets(first: 1) {
-            edges {
-              node {
-                name
-              }
-            }
-          }
+          ${UserInfo.getFragment('user')}
+          ${UserButton.getFragment('user')}
         }
       }
     `,
@@ -68,7 +92,7 @@ class ProfileRoute extends Relay.Route {
 
 ReactDOM.render(
   <Relay.RootContainer
-    Component={ProfileApp}
+    Component={ProfileViewer}
     route={new ProfileRoute()}
   />,
   document.getElementById('app')
