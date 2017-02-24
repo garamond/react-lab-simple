d20a467b8d0271e28bed0ec962519211ee91b551
Andreas Moor <github@grmnd.ch>
2017-02-24 13:05:43 +0100

    working map

--- webpack.config.js
+++ webpack.config.js
@@ -9,27 +9,21 @@ module.exports = {
     filename: 'bundle.js'
   },
   resolve: {
-    extensions: ['', '.js', '.jsx'],
+    extensions: ['.js', '.jsx'],
     alias: {
       webworkify: 'webworkify-webpack-dropin',
     }
   },
   devtool: 'source-maps',
   module: {
-    loaders: [
-      { test: /\.jsx$/, exclude: /node_modules/, loader: 'babel' },
+    rules: [
+      { test: /\.jsx$/, exclude: /node_modules/, loader: 'babel-loader' },
       { test: /\.css$/, loader: 'style-loader!css-loader' },
       { test: /\.(png|jpg)$/, loader: 'url?limit=8192' },
-      { test: /\.json$/, loader: 'json-loader' },
       {
-        include: /node_modules\/mapbox-gl.*\.js$/,
-        loader: 'transform-loader?brfs-babel',
+        include: /mapbox-gl.+\.js$/,
+        loader: 'transform-loader/cacheable?brfs-babel',
         enforce: 'post',
-      },
-      {
-        test: /node_modules\/@mapbox\/react-geocoder[\/\\].*\.js/,
-        loader: 'babel',
-        query: { presets: ['react', 'es2015'] }
       }
     ]
   },
